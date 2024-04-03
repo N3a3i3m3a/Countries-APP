c3f0c148bb89f75d5636bcfde5d3fc616e085e00
N3a3i3m3a <nishimwenaima3@gmail.com>
2024-04-03 14:23:20 +0200

    countries display

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Countries from '../components/Countries';
 import { SectionHolder } from '../styled-components/GeneralComponents';
-
+import Pagination from '../components/Pagination'
 const Home = () => {
   return (
     <div className='flex flex-col items-center'>
       <SectionHolder>
         <Countries />
       </SectionHolder>
+      <Pagination/>
     </div>
   )
 }
